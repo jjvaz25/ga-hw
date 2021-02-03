395627289653e3800acc080ecffaaf9ca48ba136
kyle liu <kyle116@yahoo.com>
2021-02-02 23:46:50 -0800

    hw3 solution added

---
+++ hw3/solution-code/homework3.js
@@ -0,0 +1,62 @@
+// 1. Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+function quarterOf(month) {
+    if (month <= 3) {
+      return 1;
+    } else if (month <= 6) {
+      return 2;
+    } else if (month <= 9) {
+      return 3;
+    } else if (month <= 12) {
+      return 4;
+    }
+}
+
+quarterOf(2)  // return 1
+quarterOf(7)  // return 3
+quarterOf(12) // return 4
+
+
+// 2. Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+function problem(x){
+    return typeof x === "number" ? x * 50 + 6 : "Error";
+}
+
+problem(2)       // return 106
+problem(10)      // return 506
+problem('hello') // return 'Error'
+
+
+// 3. Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+function sum(array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total = total + array[i];
+  }
+  return total;
+}
+
+sum([1, 5.2, 4, 0, -1]) // return 9.2
+sum([])                 // return 0
+sum([-2])               // return -2
+
+
+// 4. Write a function that returns true or false if the number passed in is even
+function testEven(n) {
+    return n % 2 === 0;
+}
+
+testEven(8)   // return true
+testEven(7)   // return false
+testEven(100) // return true
+
+
+// 5. Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0
+}
+
+isDivisible(10, 2, 5)   // return true
+isDivisible(9, 2, 4)    // return false
+isDivisible(100, 20, 5) // return true
