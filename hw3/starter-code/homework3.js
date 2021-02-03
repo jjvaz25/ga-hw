e887d4a6b1b5467b06a1babefa6e6f13d016ea32
kyle liu <kyle116@yahoo.com>
2021-02-02 23:46:17 -0800

    hw3 added

---
+++ hw3/starter-code/homework3.js
@@ -0,0 +1,51 @@
+// 1. Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+function quarterOf(month) {
+    // Your code here
+    
+}
+
+quarterOf(2)  // return 1
+quarterOf(7)  // return 3
+quarterOf(12) // return 4
+
+
+// 2. Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+function problem(x){
+    //your code here
+}
+
+problem(2)       // return 106
+problem(10)      // return 506
+problem('hello') // return 'Error'
+
+
+// 3. Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+function sum(array) {
+    //your code here
+}
+  
+sum([1, 5.2, 4, 0, -1]) // return 9.2
+sum([])                 // return 0
+sum([-2])               // return -2
+
+
+// 4. Write a function that returns true or false if the number passed in is even
+function testEven(n) {
+    //your code here
+}
+
+testEven(8)   // return true
+testEven(7)   // return false
+testEven(100) // return true
+
+
+// 5. Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+function isDivisible(n, x, y) {
+    //your code here
+}
+
+isDivisible(10, 2, 5)   // return true
+isDivisible(9, 2, 4)    // return false
+isDivisible(100, 20, 5) // return true
