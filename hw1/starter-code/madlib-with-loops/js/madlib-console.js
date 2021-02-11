4e457fe211b56603cdc67bb201d0c4d05b52d1b3
Jeff Vaz <jeff.j.vaz@gmail.com>
2021-02-10 19:06:20 -0800

    move variable for h1 element out of the global space into the createStartup function

--- hw1/starter-code/madlib-with-loops/js/madlib-console.js
+++ hw1/starter-code/madlib-with-loops/js/madlib-console.js
@@ -1,11 +1,10 @@
 let startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 let startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
-let h1Element = document.getElementById('xForY');
-// let random1 = Math.floor((Math.random() * startupX.length));
-// let random2 = Math.floor((Math.random() * startupY.length));
+
 
 const createStartupBtn = document.getElementById('create');
 const createStartup = () => {
+    let h1Element = document.getElementById('xForY');
     let random1 = Math.floor((Math.random() * startupX.length));
     let random2 = Math.floor((Math.random() * startupY.length));
     let madlib = `A startup that is ${startupX[random1]} but for ${startupY[random2]}`;
@@ -13,5 +12,4 @@ const createStartup = () => {
 };
 createStartupBtn.addEventListener('click', createStartup);
 
-// console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
 
