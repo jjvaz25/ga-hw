fdab2ac080a775bd4962eedd9271a78808825e33
Jeff Vaz <jeff.j.vaz@gmail.com>
2021-02-10 19:02:39 -0800

    add createStartup function that appends madlib to h1 element

--- hw1/starter-code/madlib-with-loops/js/madlib-console.js
+++ hw1/starter-code/madlib-with-loops/js/madlib-console.js
@@ -1,7 +1,17 @@
 let startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 let startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+let h1Element = document.getElementById('xForY');
+// let random1 = Math.floor((Math.random() * startupX.length));
+// let random2 = Math.floor((Math.random() * startupY.length));
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+const createStartupBtn = document.getElementById('create');
+const createStartup = () => {
+    let random1 = Math.floor((Math.random() * startupX.length));
+    let random2 = Math.floor((Math.random() * startupY.length));
+    let madlib = `A startup that is ${startupX[random1]} but for ${startupY[random2]}`;
+    h1Element.innerHTML = madlib;
+};
+createStartupBtn.addEventListener('click', createStartup);
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+// console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+
