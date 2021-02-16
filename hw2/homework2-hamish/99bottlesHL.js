6a083d03217a01b7046ded17f0501b77d377bd90
hamishlangmusic@gmail.com <hamishlangmusic@gmail.com>
2021-02-16 16:58:45 +1100

    change has been made

---
+++ hw2/homework2-hamish/99bottlesHL.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 100; i++) {
+  let j = i - 1;
+if (i >= 2) {console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. Take one down, pass it around, ${j} bottles of beer`)} 
+else if ( i === 1) {console.log(`${i} bottle of beer on the wall, ${i} bottle of beer. Take one down, pass it around, There's no more bottles of beer on the wall!`)
+}
+  else {
+    console.log("Thank you very much, I am sufficiently hydrated")
+  }
+}
