55e89a6fd9ddfb7873fa702bec079372cbd7a293
mjproverdi <mjproverdi@gmail.com>
2021-02-18 07:40:00 -0500

    mproverdi hw

--- hw1/starter-code/madlib-with-loops/js/madlib-console.js
+++ hw1/starter-code/madlib-with-loops/js/madlib-console.js
@@ -1,7 +1,36 @@
 let startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 let startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 
+// let random1 = Math.floor((Math.random() * startupX.length));
+// let random2 = Math.floor((Math.random() * startupY.length));
+
+// console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+
+let crtBtn = document.getElementById('create');
 let random1 = Math.floor((Math.random() * startupX.length));
 let random2 = Math.floor((Math.random() * startupY.length));
+let newStartUp = `A startup that is ${startupX[random1]} and ${startupY[random2]}`;
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+crtBtn.addEventListener('click', function() {    
+    let random1 = Math.floor((Math.random() * startupX.length));
+    let random2 = Math.floor((Math.random() * startupY.length));
+    let newStartUp = `A startup that is ${startupX[random1]} and ${startupY[random2]}`;
+    document.getElementById('xForY').innerHTML = newStartUp;
+    console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+}) 
+
+// Stretch goals
+
+let svBtn = document.getElementById('save');
+let saveList = []
+
+svBtn.addEventListener('click', function() {
+    saveList.push(newStartUp);
+    console.log(saveList);
+})  
+
+let prtFav = document.getElementById('print');
+prtFav.addEventListener('click', function() {
+    document.getElementById('favorites').innerHTML = startupX + startupY
+})
+
