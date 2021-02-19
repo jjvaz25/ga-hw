6f5b652f5e6275351609a9d2a4d46c268d97212a
hamishlangmusic@gmail.com <hamishlangmusic@gmail.com>
2021-02-19 11:25:59 +1100

    Madlibs updated and HW3

--- hw1/homework1-hamish/starter-code/madlib-with-loops/js/madlib-console.js
+++ hw1/homework1-hamish/starter-code/madlib-with-loops/js/madlib-console.js
@@ -4,45 +4,41 @@ let startupY = ['Donkeys', 'Santa', 'The 50s!', 'Elves', 'some guy called Stephe
 // let random1 = Math.floor((Math.random() * startupX.length));
 // let random2 = Math.floor((Math.random() * startupY.length));
 
-const xForY = document.getElementById('xForY')
-const favStore = document.getElementById('favorites')
+const xForY = document.getElementById('xForY');
+const favStore = document.getElementById('favorites');
 let favourite = [];
 let store = [];
 
 let dummy = document.getElementById("dummy");
 
-const generate = document.querySelector('#create')
-generate.addEventListener('click', create)
+const generate = document.querySelector('#create');
+generate.addEventListener('click', create);
 
 function create(){
     let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
-    
-favourite.splice (0,100,`A startup that is <b>${startupX[random1]}</b> but for <b>${startupY[random2]}</b>`);
-
+    let random2 = Math.floor((Math.random() * startupY.length));
+    favourite.splice (0,100,`A startup that is <b>${startupX[random1]}</b> but for <b>${startupY[random2]}</b>`);
 // favourite.splice (0,100,'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
-
     xForY.innerHTML = favourite;
     dummy.remove();
-
 }
 
-let fav = document.querySelector('#save')
-fav.addEventListener('click', save)
+let fav = document.querySelector('#save');
+fav.addEventListener('click', save);
 
 function save(){
     store = document.getElementById('xForY').textContent;
 }
 
-let cat = "cat"
+let cat = "cat";
 
-let printer = document.querySelector('#print')
-printer.addEventListener('click', print)
+let printer = document.querySelector('#print');
+printer.addEventListener('click', print);
 
 function print(){
     // favStore.innerText = "";
     favStore.innerText = store;
-    console.log(store)
+    console.log(store);
 }
 
 
