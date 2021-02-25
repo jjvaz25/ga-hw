50ab1a4d38bc1ab32a733038998f5dbd4f0e3cba
miladmo68 <miladmo68@gmail.com>
2021-02-24 22:23:31 -0500

    Milad hw4

---
+++ hw4/homework3.MiladM/objects/app.js
@@ -0,0 +1,109 @@
+/////////////////////////////////////////////////////////////
+
+///////////////////////// Challenge /////////////////////////
+
+/////////////////////////////////////////////////////////////
+
+// Write your answers inside this file, at the places where it's indicated by the comments.
+
+// 1. Suppose that we wanted to create a browser-based calendar program for keeping track of things. In comments, list at least three reasonable abstractions that you might use to build this program; for each abstraction, list out at least two properties and behaviors that it would make sense for that abstraction to have. Below is an example - please write your answer in the same format.
+  // 'Car'
+  // Description: Our app manages car sales.
+  // Every 'car' has
+  // - a make
+  // - a model
+  // - a year
+  // Every 'car' can
+  // - drive
+  // - brake
+  // - park
+
+// Answer Starts Here
+let calendarObj = {
+  calender:{
+    dates: "15/4/2021",
+    days: "Monday"
+  },
+  things: {
+    things: "exercise",
+    duration: 1 ///a hour
+  }
+}
+//console.log(calendarObj.things);
+console.log(calendarObj);
+// Answer Ends Here
+
+// 2. Create an Object literal that lines up with the following description. Store it in the variable 'pet_owner', below. Be sure to give it reasonable values for each of its properties.
+
+  // 'Owner'
+  // Description: We are making an app for a veterinary clinic - it manages pets' vaccinations.
+  // Every 'owner' has:
+  // - a name
+  // - an address
+
+let pet_owner;
+
+// Answer Starts Here
+pet_owner = function(name, address) {
+  this.name = name;
+  this.address = address;
+
+}
+let pet_owner1 = new pet_owner('Milad', 'Toronto');
+console.log('pet_owner name: '+ pet_owner1.name + '  address: ' + pet_owner1.address);
+///new
+let pet_owner2=('Milad', { street: '123 first st', city: 'Toronto' });
+console.log(pet_owner2);
+
+// Answer Ends Here
+
+// 3. Create an Object literal that lines up with the following description. Store it in the variable `some_pet`, below.
+
+  // Pet
+  // Description: We are making an app for a veterinary clinic - it manages pets' vaccinations.
+  // Every 'pet' has:
+  // - a name
+  // - a species
+  // - a breed
+  // - a noise that it can make (e.g. 'bark', 'meow', etc.)
+  // Every pet can:
+  // - make noise (each pet makes its own unique noise, as specified by `noise`.
+
+let some_pet;
+
+// Answer Starts Here
+some_pet = {
+  pet : {
+    name: "dogy",
+    species: "german",
+    breed: "german shepherd",
+    noise: "bark"
+  }
+}
+// Answer Ends Here
+
+// 4. Create a constructor function for Pet objects, as described above. The constructor function should be responseible for setting
+//    the values of `name`, `species`, `breed`, and `noise`. The constructor function should also give each pet a method called `makeNoise`,
+//    which behaves as described in Q3.
+
+let Pet;
+// Answer Starts Here
+function Pet(name, species, breed, noise) {
+  this.name = name;
+  this.species = species;
+  this.breed = breed;
+  this.noise = noise;
+}
+// Answer Ends Here
+
+module.exports = {
+  answerTwo: pet_owner,
+  answerThree: some_pet,
+  answerFour: Pet
+}
+
+/////////////////////////////////////////////////////////////
+
+// You're done! Refer back to README.md.
+
+/////////////////////////////////////////////////////////////
