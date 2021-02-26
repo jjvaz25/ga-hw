90441ea046b370de700f3cbb7eeb5a059a6c25d2
kyle liu <kyle116@yahoo.com>
2021-02-25 20:31:09 -0800

    update to solution

--- hw5/solution-code/script.js
+++ hw5/solution-code/script.js
@@ -9,6 +9,16 @@ for (let i = 100; 0 < i; i--) {
                 <li>No more bottles of beer on the wall!</li>
             </ul>
         `);
+    } else if (i === 2) {
+        $('body').append(`
+            <ul>
+                <li>${i} bottles of beer on the wall,</li>
+                <li>${i} bottles of beer!</li>
+                <li>Take one down,</li>
+                <li>Pass it around,</li>
+                <li>${i - 1} bottle of beer on the wall!</li>
+            </ul>
+        `);
     } else {
         $('body').append(`
             <ul>
