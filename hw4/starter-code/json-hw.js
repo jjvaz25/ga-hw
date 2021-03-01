e5460683d8d1fb8207fd09fb7e4213810cf38a24
mjproverdi <mjproverdi@gmail.com>
2021-02-28 21:14:25 -0500

    mikeP hw4

--- hw4/starter-code/json-hw.js
+++ hw4/starter-code/json-hw.js
@@ -8,25 +8,36 @@ var jsonString = '{ "title": "Uploads from everyone", "link": "https://www.flick
 called `flickerObj`
 */
 
-
-
+let flickerObj = JSON.parse(jsonString);
+console.log(flickerObj);
 
 /*
 2. Iterate (this means 'use a loop') and do a console.log of each of the item titles
 with the date the photo was taken. The first console.log should return the following:
 
 "Edelweiss Air, Airbus A320-214, HB-IJU 2016-02-21T11:27:58-08:00"
-
 */
 
+for (i = 0; i < flickerObj.items.length; i++) {
+    // console.log(flickerObj.items[i]);
+    let obj = flickerObj.items[i];
+    console.log(`${obj.title},  ${obj.date_taken}`);   
+}
 
 /*
 3. Create a new array called 'links'. Iterate through the items array in flickerObj and store the the links to each item in the 'links' array.
 */
 
-
+let links = [];
+for (let i = 0; i < flickerObj.items.length; i++){
+    links.push(flickerObj.items[i].link); 
+};
+console.log(links);
 
 /*
 4. Convert the 'links' array into a JSON string. Do a console.log of that JSON string, copy the results from the terminal and paste them into the input box at http://jsonlint.com/
 Did it create a valid JSON string?
 */ 
+
+linksJson = JSON.stringify(links);
+console.log(linksJson);
